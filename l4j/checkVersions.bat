638f82e6b697c8361a1b51251063707e01a3bf24
grzegok <grzegok@users.sourceforge.net>
2015-03-01 20:00:55 +0100

    Added optional maven build.

--- l4j/checkVersions.bat
+++ l4j/checkVersions.bat
@@ -2,4 +2,6 @@
 type ..\src\launch4j.properties
 findstr /C:"#define VERSION" ..\head_src\head.h 
 findstr /C:"!define PRODUCT_VERSION" nsis\launch4j.nsi
+findstr /C:"versionNumber>" ..\maven\pom.xml
+findstr /C:"version>3." ..\maven\pom.xml
 findstr /C:"Copyright (c)" nsis\launch4j-setup-license.txt
